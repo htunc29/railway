f6b58a7a446e3251875ae225bb3afa95d81d0bf2
Hüseyin Tunç <huseyint428@gmail.com>
2025-05-19 11:26:50 +0300

    Veritabanı entegrasyonu eklendi; görevler artık SQLite ile yönetiliyor. Todo modeli oluşturuldu ve görev ekleme/silme işlemleri güncellendi. Gerekli bağımlılıklar güncellendi.

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.secret_key = 'gizli_anahtar'  # Session için gerekli
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if 'todos' not in session:
-        session['todos'] = []
     if request.method == 'POST':
         yeni_todo = request.form.get('todo')
         if yeni_todo:
-            session['todos'].append(yeni_todo)
-            session.modified = True
+            todo = Todo(text=yeni_todo)
+            db.session.add(todo)
+            db.session.commit()
         return redirect(url_for('home'))
-    return render_template('index.html', todos=session['todos'])
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
 
-@app.route('/sil/<int:index>')
-def sil(index):
-    if 'todos' in session and 0 <= index < len(session['todos']):
-        session['todos'].pop(index)
-        session.modified = True
+@app.route('/sil/<int:id>')
+def sil(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
